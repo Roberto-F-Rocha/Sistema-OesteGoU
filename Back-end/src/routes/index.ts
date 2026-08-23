b8d04f618f28da5b7edd5edc45f941a763856a2e
Roberto_F.rocha <115853601+Roberto-F-Rocha@users.noreply.github.com>
2026-08-22 21:12:08 -0300

    refactor: use guarded admin user status controller

--- Back-end/src/routes/index.ts
+++ Back-end/src/routes/index.ts
@@ -8,6 +8,7 @@ import { subscribePush, unsubscribePush } from "../controllers/pushController";
 import { sendPush, getPushHistory } from "../controllers/adminPushController";
 import { getAnalyticsDashboard } from "../controllers/adminAnalyticsController";
 import { listUniversities, createUniversity, updateUniversity } from "../controllers/universityController";
+import { updateUserStatus } from "../controllers/adminUserController";
 import { auth } from "../middlewares/auth";
 import { cityAccess } from "../middlewares/cityAccess";
 import { requireRole } from "../middlewares/permissions";
@@ -18,7 +19,7 @@ import { uploadDocument } from "../controllers/uploadController";
 import { listCityAgreements, createCityAgreement, updateCityAgreementStatus, listCities } from "../controllers/cityAgreementController";
 import { createReservation, createRoundTripReservation, getMyReservations, cancelReservation, confirmReservation } from "../controllers/reservationController";
 import { createMaintenanceTicket, getMyMaintenanceTickets, listMaintenanceTickets, updateMaintenanceTicket } from "../controllers/maintenanceTicketController";
-import { getAdminDashboard, listAdminUsers, updateUserStatus, createDriver, listVehicles, createVehicle, updateVehicle, listSchedules, createSchedule, updateSchedule, listPickupPoints, createPickupPoint, updatePickupPoint, listRoutes, createRoute, updateRoute, listAuditLogs } from "../controllers/adminController";
+import { getAdminDashboard, listAdminUsers, createDriver, listVehicles, createVehicle, updateVehicle, listSchedules, createSchedule, updateSchedule, listPickupPoints, createPickupPoint, updatePickupPoint, listRoutes, createRoute, updateRoute, listAuditLogs } from "../controllers/adminController";
 
 const router = Router();
 const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 8 * 1024 * 1024 } });
